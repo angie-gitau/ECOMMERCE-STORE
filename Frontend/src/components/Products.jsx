c5ef2586e0ea82b0d2f2eede83ef2b25123fcab4
Angela Gitau <angelagitau38@gmail.com>
2025-06-24 15:05:52 +0300

    completed homepage

---
+++ Frontend/src/components/Products.jsx
@@ -0,0 +1,18 @@
+import Product from "./Product";
+
+const Products = () => {
+  return (
+    <div className="grid grid-cols-4 gap-6 px-[30px]">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+};
+
+export default Products;
